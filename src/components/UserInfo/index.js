080ec299ad39ec785d0ac83be750c0e2a5601a9e
Jagan-Konda <jagankonda83@gmail.com>
2025-12-26 15:15:36 +0000

    first commit

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="profile-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="profile-icon"
+    />
+    <h1 className="name">Wade Warren</h1>
+    <p className="designation">Software Developer at UK</p>
+  </div>
+)
+
+export default UserInfo
